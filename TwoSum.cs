a2f3ac5497f4c6572ac5ec3859f976d223b6fe9d
aketo <aketo14@7.fackme.gq>
2017-03-21 21:27:25 -0400

    Solve problem #1

---
+++ TwoSum.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var hashMap = new Dictionary<int, int>();
+        
+        for (int i = 0; i < nums.Length; i++) {
+            if (hashMap.ContainsKey(target - nums[i])) {
+                var first = hashMap[target-nums[i]];
+                var second = i;
+                return new int[] { first, second };
+            } else {
+                if (hashMap.ContainsKey(nums[i])) {
+                    hashMap[nums[i]] = i;
+                } else {
+                    hashMap.Add(nums[i], i);
+                }
+            }
+        }
+        
+        return new int[] {};
+    }
+}
